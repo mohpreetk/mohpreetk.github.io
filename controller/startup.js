862668ce00b925330f8cced13f4d0eb832ded8b2
Mohpreet Kaur <67534712+mohpreetk@users.noreply.github.com>
2022-01-22 17:12:13 -0500

    Faster Animation

--- controller/startup.js
+++ controller/startup.js
@@ -8,8 +8,23 @@ var links = document.getElementById("links");
 var id = null;
 
 window.onload = function () {
+<<<<<<< Updated upstream
   checkbox.checked = false
   setTimeout(moveH1, 500);
+=======
+  checkbox.checked = false;
+  if (pageVisited == null) {
+    sessionStorage.setItem("homepage.visited", 1);
+    setTimeout(moveH1, 3);
+  } else {
+    h1.style.top = "0px";
+    h1.style.left = "0px";
+    links.style.top = "0px";
+    links.style.right = "0px";
+    links.style.display = "inline";
+    showOthers();
+  }
+>>>>>>> Stashed changes
 };
 
 function moveH1() {
@@ -17,23 +32,29 @@ function moveH1() {
   var h1PosLeft = h1Rect.left;
   var h1PosTop = h1Rect.top;
   clearInterval(id);
-  id = setInterval(frame, 2);
+  id = setInterval(frame, 0.002);
   function frame() {
-    if (h1PosTop <= 1 && h1PosLeft <= 1) {
+    if (h1PosTop <= 3 && h1PosLeft <= 3) {
       h1.style.top = "0px";
       h1.style.left = "0px";
       clearInterval(id);
       links.style.display = "inline";
+<<<<<<< Updated upstream
       setTimeout(moveLinks, 500);
     } else if (h1PosTop <= 1) {
       h1PosLeft--;
+=======
+      setTimeout(moveLinks, 3);
+    } else if (h1PosTop <= 3) {
+      h1PosLeft-=3;
+>>>>>>> Stashed changes
       h1.style.left = h1PosLeft + "px";
-    } else if (h1PosLeft <= 1) {
-      h1PosTop--;
+    } else if (h1PosLeft <= 3) {
+      h1PosTop-=3;
       h1.style.top = h1PosTop + "px";
     } else {
-      h1PosLeft--;
-      h1PosTop--;
+      h1PosLeft-=3;
+      h1PosTop-=3;
       h1.style.top = h1PosTop + "px";
       h1.style.left = h1PosLeft + "px";
     }
@@ -46,22 +67,22 @@ function moveLinks() {
   var linksPosRight = bodyRect.right - linksRect.right;
   var linksPosTop = linksRect.top;
   clearInterval(id);
-  id = setInterval(frame, 2);
+  id = setInterval(frame, 0.002);
   function frame() {
-    if (linksPosTop <= 1 && linksPosRight <= 1) {
+    if (linksPosTop <= 3 && linksPosRight <= 3) {
       links.style.top = "0px";
       links.style.right = "0px";
       clearInterval(id);
       showOthers();
-    } else if (linksPosTop <= 1) {
-      linksPosRight--;
+    } else if (linksPosTop <= 3) {
+      linksPosRight-=3;
       links.style.right = linksPosRight + "px";
-    } else if (linksPosRight <= 1) {
-      linksPosTop--;
+    } else if (linksPosRight <= 3) {
+      linksPosTop-=3;
       links.style.top = linksPosTop + "px";
     } else {
-      linksPosRight--;
-      linksPosTop--;
+      linksPosRight-=3;
+      linksPosTop-=3;
       links.style.top = linksPosTop + "px";
       links.style.right = linksPosRight + "px";
     }
